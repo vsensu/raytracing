0610d2001ccdb4896fe582a55894bcf187865cc8
vsensu <vsensu@foxmail.com>
2021-05-26 10:49:20 +0800

    code format

--- src/math/main.cpp
+++ src/math/main.cpp
@@ -1,21 +1,20 @@
 #include <iostream>
 #include "vec.h"
 
-int main()
-{
-    vec3 f(1, 2, 3);
-    std::cout << f << '\n';
-    vec3 f2 = -f;
-    std::cout << f2 << '\n';
-    f += f2;
-    std::cout << f << '\n';
-    f -= f2;
-    std::cout << f << '\n';
-    f *= 3;
-    std::cout << f << '\n';
-    f /= 3;
-    std::cout << f << '\n';
-    std::cout << f.length() << '\n';
-    std::cout << f.length_squared() << '\n';
-    return 0;
+int main() {
+  vec3 f(1, 2, 3);
+  std::cout << f << '\n';
+  vec3 f2 = -f;
+  std::cout << f2 << '\n';
+  f += f2;
+  std::cout << f << '\n';
+  f -= f2;
+  std::cout << f << '\n';
+  f *= 3;
+  std::cout << f << '\n';
+  f /= 3;
+  std::cout << f << '\n';
+  std::cout << f.length() << '\n';
+  std::cout << f.length_squared() << '\n';
+  return 0;
 }
--- src/math/vec.h
+++ src/math/vec.h
@@ -1,115 +1,92 @@
-#ifndef VEC_H_H_H
-#define VEC_H_H_H
+#ifndef MATH_VEC_H_
+#define MATH_VEC_H_
 
 #include <cmath>
-#include <utility>
-#include <ostream>
 #include <functional>
 #include <iostream>
+#include <ostream>
+#include <utility>
 
 template <typename ElementType, int N>
-class vec
-{
-public:
-    template <typename... Tail>
-    vec(typename std::enable_if<sizeof...(Tail) + 1 == N, ElementType>::type head, Tail... tail)
-        : elems_{head, ElementType(tail)...}
-    {
+class vec {
+ public:
+  template <typename... Tail>
+  vec(typename std::enable_if<sizeof...(Tail) + 1 == N, ElementType>::type head,
+      Tail... tail)
+      : elems_{head, ElementType(tail)...} {}
+
+  auto operator-() const { return minus(std::make_index_sequence<N>{}); }
+  ElementType operator[](int i) const { return elems_[i]; }
+  ElementType& operator[](int i) { return elems_[i]; }
+
+  friend std::ostream& operator<<(std::ostream& os, const vec& v) {
+    // v.print(os, std::make_index_sequence<N>{});
+    auto print_elem = [&](const ElementType& e) { os << e << ' '; };
+    v.transform(print_elem);
+    return os;
+  }
+
+  // template <std::size_t... I>
+  // void print(std::ostream &os, std::index_sequence<I...>) const
+  // {
+  //     ((os << elems_[I] << ' '), ...);
+  // }
+
+  vec& operator+=(const vec& v) {
+    for (int i = 0; i < N; ++i) {
+      elems_[i] += v.elems_[i];
     }
+    return *this;
+  }
 
-    auto operator-() const
-    {
-        return minus(std::make_index_sequence<N>{});
-    }
-    ElementType operator[](int i) const { return elems_[i]; }
-    ElementType &operator[](int i) { return elems_[i]; }
+  vec& operator-=(const vec& v) { return this->operator+=(-v); }
 
-    friend std::ostream &operator<<(std::ostream &os, const vec &v)
-    {
-        // v.print(os, std::make_index_sequence<N>{});
-        auto print_elem = [&](const ElementType &e) { os << e << ' '; };
-        v.transform(print_elem);
-        return os;
-    }
+  vec& operator*=(double t) {
+    for (int i = 0; i < N; ++i) {
+      elems_[i] *= t;
+    };
+    return *this;
+  }
 
-    // template <std::size_t... I>
-    // void print(std::ostream &os, std::index_sequence<I...>) const
-    // {
-    //     ((os << elems_[I] << ' '), ...);
-    // }
+  vec& operator/=(double t) { return this->operator*=(1 / t); }
 
-    vec &operator+=(const vec &v)
-    {
-        for (int i = 0; i < N; ++i)
-        {
-            elems_[i] += v.elems_[i];
-        }
-        return *this;
-    }
+  ElementType length() const { return std::sqrt(length_squared()); }
 
-    vec &operator-=(const vec &v)
-    {
-        return this->operator+=(-v);
-    }
+  ElementType length_squared() const {
+    ElementType temp{};
+    auto square = [&](const ElementType& e) { temp += e * e; };
+    transform(square);
+    return temp;
+  }
 
-    vec &operator*=(double t)
-    {
-        for (int i = 0; i < N; ++i)
-        {
-            elems_[i] *= t;
-        };
-        return *this;
-    }
+ private:
+  template <std::size_t... I>
+  vec minus(std::index_sequence<I...>) const {
+    return vec(-elems_[I]...);
+  }
 
-    vec &operator/=(double t)
-    {
-        return this->operator*=(1/t);
-    }
+  void transform(std::function<void(const ElementType&)> p) const {
+    transform_inner(p, std::make_index_sequence<N>{});
+  }
 
-    ElementType length() const
-    {
-        return std::sqrt(length_squared());
-    }
+  template <std::size_t... I>
+  void transform_inner(std::function<void(const ElementType&)> p,
+                       std::index_sequence<I...>) const {
+    ((p(elems_[I])), ...);
+  }
 
-    ElementType length_squared() const
-    {
-        ElementType temp{};
-        auto square = [&](const ElementType &e) { temp += e * e; };
-        transform(square);
-        return temp;
-    }
+  void transform(std::function<void(ElementType&)> p) {
+    transform_inner(p, std::make_index_sequence<N>{});
+  }
 
-private:
-    template <std::size_t... I>
-    vec minus(std::index_sequence<I...>) const
-    {
-        return vec(-elems_[I]...);
-    }
+  template <std::size_t... I>
+  void transform_inner(std::function<void(ElementType&)> p,
+                       std::index_sequence<I...>) {
+    ((p(elems_[I])), ...);
+  }
 
-    void transform(std::function<void(const ElementType &)> p) const
-    {
-        transform_inner(p, std::make_index_sequence<N>{});
-    }
-
-    template <std::size_t... I>
-    void transform_inner(std::function<void(const ElementType &)> p, std::index_sequence<I...>) const
-    {
-        ((p(elems_[I])), ...);
-    }
-
-    void transform(std::function<void(ElementType &)> p)
-    {
-        transform_inner(p, std::make_index_sequence<N>{});
-    }
-
-    template <std::size_t... I>
-    void transform_inner(std::function<void(ElementType &)> p, std::index_sequence<I...>)
-    {
-        ((p(elems_[I])), ...);
-    }
-
-private:
-    ElementType elems_[N]{0};
+ private:
+  ElementType elems_[N]{0};
 };
 
 using vec2f = vec<float, 2>;
@@ -123,4 +100,4 @@ using vec2 = vec2f;
 using vec3 = vec3f;
 using vec4 = vec4f;
 
-#endif
+#endif  // MATH_VEC_H_
